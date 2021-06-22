afcc986d05f84e1911a1df74e8a32187fdc5e310
Eva Karlsdottir <evakarlsdottir@gmail.com>
2021-06-22 21:08:25 +0200

    minor text modification

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         <h1 class="text-2xl sm:text-6xl my-8">Hello, I'm Eva 👋</h1>
         <h2 class="text-sm font-light sm:text-4xl">I create beautiful things online.</h2>
       </div>
-        <h3 class="font-light text-sm sm:text-4xl my-10">I'm currently building my portfolio but in the meantime you can find me on the platforms below</h3>
+        <h3 class="font-light text-sm sm:text-4xl my-10">I'm currently building my portfolio but in the meantime you can find me on the platforms here below</h3>
         <div class="motion-safe:animate-bounce text-4xl sm:text-5xl mb-5">👇</div>
     </section>
 
